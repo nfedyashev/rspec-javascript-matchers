b06479530b43fdd89e2b409e85a30b87169857ea
Nikita Fedyashev <nfedyashev@gmail.com>
2018-06-17 04:00:17 +0300

    Distinguish runtime from parse error for both methods - therubyracer(v8) parse & mini_racer

--- lib/rspec-javascript-matchers.rb
+++ lib/rspec-javascript-matchers.rb
@@ -16,9 +16,14 @@ RSpec::Matchers.define :be_valid_javascript do
         cxt.eval(actual)
         ''
       rescue V8::Error => e
+        #e.in_javascript? => true
+
+        # this looks more like a runtime error rather than parse error
+        return '' if e.root_cause.to_s.include?('is not defined')
+
         # Because <eval> doesn't provide any meaningful context info here:k:w
         # Invalid regular expression: missing / at <eval>:4:12
-        e.root_cause.message.gsub('<eval>:', '')
+        e.root_cause.message.gsub('<eval>:', '') + "\nActual:\n#{actual}"
       end
     elsif defined?(MiniRacer)
       context = MiniRacer::Context.new
@@ -28,7 +33,10 @@ RSpec::Matchers.define :be_valid_javascript do
       rescue MiniRacer::ParseError => e
         # Because default is:
         # "Uncaught SyntaxError: Invalid regular expression: missing / at undefined:4:10"
-        e.message.gsub('Uncaught SyntaxError: ', '').gsub(' undefined:', ' ')
+        e.message.gsub('Uncaught SyntaxError: ', '').gsub(' undefined:', ' ') + "\nActual:\n#{actual}"
+      rescue MiniRacer::RuntimeError => e
+        # Most likely smth like: "ReferenceError: $ is not defined" which we don't care about in specs
+        ''
       end
     else
       %(Maker sure you have "mini_racer" or "therubyracer" gem in your Gemfile dependencies)
